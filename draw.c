8b7f0fa9ed77468978bf5dabadee8fd268a8431a
dennisYatunin <dyatun@gmail.com>
2016-02-24 01:31:24 -0500

    Basically done

--- draw.c
+++ draw.c
@@ -3,23 +3,28 @@
 #include "draw.h"
 
 void draw_line(screen s, int x0, int y0, int x1, int y1, int value) {
-	int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
-	int dy = abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
-	int err = (dx > dy ? dx : -dy) / 2, e2;
+	int dx = abs(x1 - x0);
+	int dy = abs(y1 - y0);
+
+	int x_step = x0 < x1 ? 1 : -1;
+	int y_step = y0 < y1 ? 1 : -1;
+
+	int error = (dx > dy ? dx : -dy) / 2;
+	int original_error;
 
 	while (1) {
 		plot(s, x0, y0, value);
 		if (x0 == x1 && y0 == y1) {
-			break;
+			return;
 		}
-		e2 = err;
-		if (e2 > -dx) {
-			err -= dy;
-			x0 += sx;
+		original_error = error;
+		if (original_error > -dx) {
+			error -= dy;
+			x0 += x_step;
 		}
-		if (e2 < dy) {
-			err += dx;
-			y0 += sy;
+		if (original_error < dy) {
+			error += dx;
+			y0 += y_step;
 		}
 	}
 }
